39f5edcfaa9828004ea2291e6492794d2b5dc287
alexeiakimenko <alexeiakimenko@ya.ru>
2023-08-08 18:52:37 +0700

    change

--- gamer/aboutgames/new_games/urls.py
+++ gamer/aboutgames/new_games/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.new_games, name='new-games')
+    path('', views.new_games, name='new-games'),
+    path('single-new-game/<str:pk>/', views.single_new_game, name='single-new-game'),
 ]
--- gamer/aboutgames/new_games/views.py
+++ gamer/aboutgames/new_games/views.py
@@ -8,3 +8,11 @@ def new_games(request):
         'new_games': new_game
     }
     return render(request, 'new_games/new_games.html', context)
+
+
+def single_new_game(request, pk):
+    new_game = NewGame.objects.get(id=pk)
+    context = {
+        'new_game': new_game,
+    }
+    return render(request, 'new_games/single_new_game.html',context)
