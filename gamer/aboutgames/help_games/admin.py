7328850210efb2e10cb5fee8b5f0a395cbd39afd
alexeiakimenko <alexeiakimenko.ya.ru>
2023-09-23 16:41:33 +0700

    change

--- gamer/aboutgames/help_games/admin.py
+++ gamer/aboutgames/help_games/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from ckeditor_uploader.widgets import CKEditorUploadingWidget
 from django import forms
-from .models import Game, Hint, VideoView,GameComment
+from .models import Game, Hint, VideoView, GameComment
 
 
 class HintAdminForm(forms.ModelForm):
@@ -15,6 +15,7 @@ class HintAdminForm(forms.ModelForm):
 class GameAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'genre', 'release_date')
     list_display_links = ('title',)
+
     search_fields = ('title',)
 
 
@@ -22,16 +23,18 @@ class HintAdmin(admin.ModelAdmin):
     form = HintAdminForm
     list_display = ('hint_game', 'hint_title', 'hint_description')
     list_display_links = ('hint_title',)
-    search_fields = ('hint_game',)
+    list_filter = ('hint_game',)
+    search_fields = ('hint_title',)
 
 
 class VideoViewAdmin(admin.ModelAdmin):
     list_display = ('video_game', 'video_title', 'video_description')
     list_display_links = ('video_title',)
-    search_fields = ('video_game',)
+    search_fields = ('video_title',)
+    list_filter = ('video_game',)
 
 
 admin.site.register(Game, GameAdmin)
 admin.site.register(Hint, HintAdmin)
-admin.site.register(VideoView,VideoViewAdmin)
+admin.site.register(VideoView, VideoViewAdmin)
 admin.site.register(GameComment)
